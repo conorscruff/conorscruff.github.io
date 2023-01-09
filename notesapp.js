11021d92dd6f04b94e04eaf31c69ec70d07ab3ee
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-01-09 10:49:19 +0800

    Create notesapp.js

---
+++ notesapp.js
@@ -0,0 +1,15 @@
+const notes = [{
+title: 'my next trip', 
+body: 'I would like to go to Spain'
+},{
+title: 'Habits to work on', 
+body: 'Exercise. Eating a little better.'
+},{
+title: 'Office modification',
+body: 'Get a new seat'
+}]
+
+document.querySelector('button').addEventListener('click', function(e){
+console.log('Did this work?')
+e.target.textContent = 'The button was clicked.'
+})
