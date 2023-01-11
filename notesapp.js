09b9d8e45777ec0e72af5705e22116d3eead04e5
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-01-11 17:09:19 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -23,10 +23,6 @@ const renderNotes = function(notes, filters){
   document.querySelector('body').appendChild(noteEl)
  })
 }
-const removeNotes = function(notes){
- document.querySelectorAll('p').forEach(function(note){
-  note.remove()
- })
 renderNotes(notes, filters)
 document.querySelector('#create').addEventListener('click', function(e){
 console.log('Did this work?')
