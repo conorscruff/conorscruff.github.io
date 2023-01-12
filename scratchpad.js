8575a4df0277e619c76091f28311025e79897b6c
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-01-12 11:12:09 +0800

    Update scratchpad.js

--- scratchpad.js
+++ scratchpad.js
@@ -1,10 +1,1 @@
-const array = [1, 2, 3, 4, 6]
 
-const filters = {
-  searchText: 2
-}
-function filterItems(array, filters) {
-  return array.filter((el) => el = filters.searchText);
-}
-console.log(array);
-console.log(filterItems(array, filters));
