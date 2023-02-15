08150c9f0ba96c755f60d3e083ced33008d8a124
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-02-15 13:45:39 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -32,6 +32,8 @@ notes.push({
 title:'',
 body: ''
 })
+localStorage.setItem('notes', JSON.stringify(notes));
+ renderNotes(notes, filters);
 })
 document.querySelector('#search-box').addEventListener('input', function(e){
   filters.searchText = e.target.value
