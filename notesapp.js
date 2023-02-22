59734cc2a038898811a03344818d9f9badacdeb2
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-02-22 14:18:47 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -1,5 +1,23 @@
-const renderNotes = function(notes, filters){ const filteredNotes = notes.filter(function(note){return note.title.toLowerCase().includes(filters.searchText.toLowerCase())}) document.querySelector('#notes').innerHTML = '' filteredNotes.forEach(function(note){ const noteEl = document.createElement('p') noteEl.textContent = note.title document.querySelector('#notes').appendChild(noteEl)})} renderNotes(notes, filters)
-renderNotes(notes, filters);
+const notes =[] 
+const filters ={ 
+searchText: '' 
+} 
+const notesJSON = localStorage.getItem('notes') 
+if(notesJSON != null) { 
+
+}
+const renderNotes = function(notes, filters){ 
+const filteredNotes = notes.filter(function(note){return note.title.toLowerCase().includes(filters.searchText.toLowerCase())}) 
+
+document.querySelector('#notes').innerHTML = '' 
+filteredNotes.forEach(function(note){ 
+const noteEl = document.createElement('p') 
+noteEl.textContent = note.title 
+document.querySelector('#notes').appendChild(noteEl)
+}
+)} 
+renderNotes(notes, filters)
+
 
 document.querySelector('#create').addEventListener('click', function(e){
 notes.push({
