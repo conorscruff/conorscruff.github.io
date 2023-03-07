788338b4d40ddd58c3bdb0f7e2e8dcef4bcae233
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-07 17:01:58 +0800

    Update todoapp.js

--- todoapp.js
+++ todoapp.js
@@ -5,38 +5,6 @@ const filters = {
   searchText: '',
   hideCompleted: false
 }
-//function renderTodos, returns true if todo.text matches searchText
-const renderTodos = function(todos, filters){ 
-  const filteredTodos = todos.filter(function(todo){
-    if(filters.hideCompleted){
-      return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())&& !todo.completed;
-    }
-    else{
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-    }
-  })
-  //renders filteredTodos in console
-  console.log(filteredTodos);
-  // we create a new array of incomplete todos on the basis of their completed attribute
-const incompleteTodos = filteredTodos.filter(function(todo) {
-return !todo.completed
-})
- //we first wipe the innerHTML of our todo-list div
-  document.querySelector('#todo-list').innerHTML = '';
-//we create a summary heading and define the text, along with the length of incompleteTodos array
-const summary = document.createElement('h2')
-summary.textContent = 'Todos yet to complete: ' + incompleteTodos.length
-
-//we append this summary heading to the div in our document
-document.querySelector('#todo-list').appendChild(summary)
- 
-  //we iterate through filteredTodos, creating a new element for each and making the content of each the todo, appending this to our div.
-  filteredTodos.forEach(function(todo){
-    const todoEl = generateTodoDOM(todo);
-    document.querySelector('#todo-list').appendChild(todoEl);
-  })
-  
-}
 //the initial call of renderTodos
 renderTodos(todos, filters);
 
