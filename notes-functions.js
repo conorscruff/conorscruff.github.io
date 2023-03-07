c8f218a7684bf418896ced9f54ff6fcc51542f60
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-07 15:47:18 +0800

    Update notes-functions.js

--- notes-functions.js
+++ notes-functions.js
@@ -9,7 +9,7 @@ return JSON.parse(notesJSON)
   }
 }
 //Generate the DOM structure for a note
-const generateNoteDOM(note){
+const generateNoteDOM = function(note){
 const noteEl = document.createElement('p') 
 if (note.title.length > 0) { 
  noteEl.textContent = note.title
