e0d851f4611463a35598ec9b7d9f127350184fd3
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-07 10:34:07 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -7,7 +7,9 @@ if(notesJSON != null) {
 notes = JSON.parse(notesJSON)
 }
 const renderNotes = function(notes, filters){ 
-const filteredNotes = notes.filter(function(note){return note.title.toLowerCase().includes(filters.searchText.toLowerCase())}) 
+const filteredNotes = notes.filter(function(note){
+ return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+}) 
 
 document.querySelector('#list-notes').innerHTML = '' 
 filteredNotes.forEach(function(note){ 
