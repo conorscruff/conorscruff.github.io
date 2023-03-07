bf41980e36dac786700b672eea350cb606132f1e
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-07 16:32:53 +0800

    Update todo-functions.js

--- todo-functions.js
+++ todo-functions.js
@@ -1,1 +1,12 @@
-
+//Parse any todos saved in local storage or return an empty array
+const getSavedTodos = function(){
+  const todosJSON = localStorage.getItem('todos');
+//if present
+if(todosJSON !== null) {
+  //we parse the info to todos array
+  return JSON.parse(todosJSON)
+}
+  else{
+    return []
+  }
+}
