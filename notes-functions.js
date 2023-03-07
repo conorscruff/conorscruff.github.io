b515c5f2da3982400943f3c0508c79dadab958b2
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-07 15:33:14 +0800

    Update notes-functions.js

--- notes-functions.js
+++ notes-functions.js
@@ -1,1 +1,8 @@
+//Read existing data from localStorage
+const getSavedNotes = function(){
+  const notesJSON = localStorage.getItem('notes') 
+if(notesJSON !== null) { 
+notes = JSON.parse(notesJSON)
+}
+}
 
