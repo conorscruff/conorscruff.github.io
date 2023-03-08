832e2e4008cf50b88cf5e0657c53591f882d7519
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-08 16:25:18 +0800

    Update todo-functions.js

--- todo-functions.js
+++ todo-functions.js
@@ -29,6 +29,9 @@ const generateTodoDOM = function(todo){
   const todoCheckbox = document.createElement('input');
   todoCheckbox.setAttribute('type', 'checkbox');
   todoCheckbox.checked = todo.completed;
+  todoCheckbox.addEventListener('change', function(e){
+  todoCheckbox.checked = !todoCheckbox.checked;
+  })
  
   todoEl.appendChild(todoCheckbox);
   const todoText = document.createElement('span');
