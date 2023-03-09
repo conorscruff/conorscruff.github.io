39d84a8c25647fd673e0eabab185312360ef108c
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-09 12:28:30 +0800

    Update notes-edit.js

--- notes-edit.js
+++ notes-edit.js
@@ -1,2 +1,7 @@
 const noteHash = location.hash.substring(1);
-console.log(noteHash);
+const notes = getSavedNotes();
+const note = notes.find(function(note){
+  return noteHash === note.id;
+}
+if(note === "undefined"){
+  location.assign('/index.html');
