9fdee8d4e00c70b50b24542bb5b48ff2ad593e51
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-09 15:28:29 +0800

    Update notes-edit.js

--- notes-edit.js
+++ notes-edit.js
@@ -1,13 +1,16 @@
-const noteHash = location.hash.substring(1);
-const notes = getSavedNotes();
-let note = notes.find(function(note){
-  return noteHash === note.id;
-})
-if(note === undefined){
-  location.assign('/notes.html');
-}
 document.querySelector('#note-title').value = note.title;
 document.querySelector('#note-body').value = note.body;
 document.querySelector('#note-title').addEventListener('change', function(e){
-console.log(e.target.value);
+const noteHash = location.hash.substring(1);
+const notes = getSavedNotes();
+const note = notes.find(function(note){
+return noteHash === note.id;
+})
+if(note === undefined){
+}
+  else{
+  note.title = e.target.value;
+  saveNotes(notes);
+  }
+   location.assign('/notes.html');
 })
