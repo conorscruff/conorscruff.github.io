60d58131dd07309a9617589f9816f9c673c74365
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-09 15:43:21 +0800

    Update notes-edit.js

--- notes-edit.js
+++ notes-edit.js
@@ -3,12 +3,14 @@ const noteHash = location.hash.substring(1);
 const notes = getSavedNotes();
 const note = notes.find(function(note){
 return noteHash === note.id;
+
 })
 if(note === undefined){
 }
   else{
+    document.querySelector('#note-title').value = note.title;
   note.title = e.target.value;
-document.querySelector('#note-title').value = note.title;
+
   saveNotes(notes);
     renderNotes(notes, filters);
   }
