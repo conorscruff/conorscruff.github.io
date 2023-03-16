8cf6bc69a59554e1fa8085037d260b077faf9902
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-16 16:21:30 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -33,6 +33,7 @@ const now = new Date('Thu Mar 16 2023 11:56:23');
 const then = new Date('Mon Oct 15 1984 11:56:23');
 if(now.getTime()<then.getTime()){
     console.log(now.toString())
+}
     else{
     console.log(then.toString())
     }
