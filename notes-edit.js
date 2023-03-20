4d30b0cd0bdd52ee9fe80d908d86e5fe86ada551
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-20 15:46:31 +0800

    Update notes-edit.js

--- notes-edit.js
+++ notes-edit.js
@@ -15,7 +15,7 @@ if(note === undefined){
 titleElement.value = note.title;
 bodyElement.value = note.body;
 createdAtElement.innerHTML = `Created at: ${note.createdAt}`;
-updatedAtElement.innerHTML = `Last updated at: ${note.updatedAt.fromNow()}`;
+updatedAtElement.innerHTML = `Last updated at: ${note.updatedAt}`;
 titleElement.addEventListener('change', function(e){
 note.title = e.target.value;
   note.updatedAt = moment().valueOf();
