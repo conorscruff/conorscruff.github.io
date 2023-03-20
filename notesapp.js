a5c3e2c013fd305b88b932059b8f97b6dfdcdc85
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-20 11:15:58 +0800

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -31,6 +31,6 @@ if(e.key==='notes'){
 })
 
 const birthday = moment();
-birthday.subtract(39, 'years').add(7, 'months').subtract(5, 'days')
+birthday.year(1984).month(9).date(15);
 
 console.log(birthday.format('ll'));
