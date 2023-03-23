937e3a5472b65e24bafabeafe519c8d9061c9b7f
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-23 15:34:27 +0800

    Update advanced-arrow.js

--- advanced-arrow.js
+++ advanced-arrow.js
@@ -3,15 +3,17 @@ const square = (num) => {
 }
 console.log(square(5));
 
-const people=[{
+const people = [{
 name: 'Conor', 
-age: 39}{
-              name: 'Bradley',
-              age: 54}{
-              name: 'Oscar',
-              age: 21}]
+age: 39}
+{
+name: 'Bradley',
+age: 54}
+{name: 'Oscar',
+age: 21}
+]
               
-              const under30 = people.filter(function(person){
-              return person.age < 30
-              })
-              console.log(under30)
+const under30 = people.filter(function(person){
+ return person.age < 30
+})
+console.log(under30)
