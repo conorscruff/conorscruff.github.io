d91cd0c5c71eeea698492017df6ee6517b1a21a9
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-24 15:02:19 +0800

    Update notes-edit.js

--- notes-edit.js
+++ notes-edit.js
@@ -6,9 +6,7 @@ const createdAtElement = document.querySelector('#created-at');
 const noteHash = location.hash.substring(1);
 const updatedAtElement = document.querySelector('#updated-at');
 let notes = getSavedNotes();
-let note = notes.find(function(note){
-  return noteHash === note.id;
-})
+let note = notes.find((note) => noteHash === note.id);
 if(note === undefined){
   location.assign('/notes.html');
 }
@@ -16,27 +14,27 @@ titleElement.value = note.title;
 bodyElement.value = note.body;
 createdAtElement.textContent = `Created at: ${note.createdAt}`;
 updatedAtElement.textContent = generateLastEdited(note.updatedAt);
-titleElement.addEventListener('change', function(e){
+titleElement.addEventListener('change', (e) => {
 note.title = e.target.value;
   note.updatedAt = moment().valueOf();
   updatedAtElement.textContent = generateLastEdited(note.updatedAt);
   saveNotes(notes);
 })
-bodyElement.addEventListener('change',function(e){
+bodyElement.addEventListener('change',(e) => {
 note.body = e.target.value;
 note.updatedAt = moment().valueOf();
   updatedAtElement.textContent = generateLastEdited(note.updatedAt);
 saveNotes(notes);
 })
-removeElement.addEventListener('click', function(){
+removeElement.addEventListener('click', () => {
   removeNote(noteHash);
   saveNotes(notes);
   location.assign('/notes.html');
 })
-saveNoteElement.addEventListener('click', function(){
+saveNoteElement.addEventListener('click', () => {
   location.assign('/notes.html');
 })
-window.addEventListener('storage', function(e){
+window.addEventListener('storage', (e) => {
 if(e.key==='notes'){
   notes = JSON.parse(e.newValue);
   note = notes.find(function(note){
