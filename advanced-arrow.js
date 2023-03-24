e674b1792eacd74993b35502702cbab1d8d15580
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-24 15:32:49 +0800

    Update advanced-arrow.js

--- advanced-arrow.js
+++ advanced-arrow.js
@@ -21,3 +21,6 @@ const add = function(a, b){
   return arguments[0] + arguments[1];
 }
 console.log(add(11,22,33,44));
+
+const team = ['Tyler', 'Johnathan', 'Rachel'];
+ console.log(team.length <= 4 ? `Team size: ${team.length})`: 'Too many members on your team')
