53b77697f209c306b7395e35382e774e21812df2
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-24 15:07:12 +0800

    Update todo-functions.js

--- todo-functions.js
+++ todo-functions.js
@@ -1,5 +1,5 @@
 //Parse any todos saved in local storage or return an empty array
-const getSavedTodos = function(){
+const getSavedTodos = () =>{
   const todosJSON = localStorage.getItem('todos');
 //if present
 if(todosJSON !== null) {
@@ -11,33 +11,29 @@ if(todosJSON !== null) {
   }
 }
 //Remove todo by ID
-const removeTodo = function(id){
-  const todoIndex = todos.findIndex(function(todo){
-  return todo.id === id;
-  })
+const removeTodo = (id) => {
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
   if(todoIndex > -1){
     todos.splice(todoIndex, 1)
   }
 }
-const toggleCheckbox = function(id){
-  const todo = todos.find(function(todo){
-  return todo.id === id;
-  })
+const toggleCheckbox = (id) => {
+  const todo = todos.find((todo) =>todo.id === id)
   if(todo !== undefined){
     todo.completed = !todo.completed;
   }
 }
 //Save todos to local storage
-const saveTodos = function(todos){
+const saveTodos = (todos) => {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
-const generateTodoDOM = function(todo){
+const generateTodoDOM = (todo) => {
  const todoEl = document.createElement('div');
  //create a checkbox element
   const todoCheckbox = document.createElement('input');
   todoCheckbox.setAttribute('type', 'checkbox');
   todoCheckbox.checked = todo.completed;
-  todoCheckbox.addEventListener('change', function(){
+  todoCheckbox.addEventListener('change', () => {
     toggleCheckbox(todo.id);
     saveTodos(todos);
     renderTodos(todos, filters);
@@ -55,7 +51,7 @@ const generateTodoDOM = function(todo){
   const removeTodoButton = document.createElement('button');
   todoEl.appendChild(removeTodoButton);
   removeTodoButton.textContent = 'x';
-  removeTodoButton.addEventListener('click', function(){
+  removeTodoButton.addEventListener('click', () => {
     removeTodo(todo.id);
     saveTodos(todos);
     renderTodos(todos, filters);
@@ -64,8 +60,8 @@ const generateTodoDOM = function(todo){
   return todoEl;
 }
 //render our todo app
-const renderTodos = function(todos, filters){ 
-  const filteredTodos = todos.filter(function(todo){
+const renderTodos = (todos, filters) => { 
+  const filteredTodos = todos.filter((todo) => {
     if(filters.hideCompleted){
       return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())&& !todo.completed;
     }
@@ -75,19 +71,17 @@ const renderTodos = function(todos, filters){
   })
 
   // we create a new array of incomplete todos on the basis of their completed attribute
-const incompleteTodos = filteredTodos.filter(function(todo) {
-return !todo.completed
-})
+const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
  document.querySelector('#todo-list').appendChild(generateSummaryDOM(incompleteTodos));
  
   //we iterate through filteredTodos, creating a new element for each and making the content of each the todo, appending this to our div.
-  filteredTodos.forEach(function(todo){
+  filteredTodos.forEach((todo) => {
     document.querySelector('#todo-list').appendChild(generateTodoDOM(todo));
   })
   
 }
 //generate Summary DOM
-const generateSummaryDOM = function(incompleteTodos){
+const generateSummaryDOM = (incompleteTodos) => {
 //we first wipe the innerHTML of our todo-list div
   document.querySelector('#todo-list').innerHTML = '';
 //we create a summary heading and define the text, along with the length of incompleteTodos array
