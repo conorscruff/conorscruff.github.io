6fb3ca2fc96530ceb104d56b9a881c763ce10f50
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-25 11:23:47 +0800

    Update advanced-arrow.js

--- advanced-arrow.js
+++ advanced-arrow.js
@@ -28,3 +28,5 @@ team.push('Jordan');
 team.push('Brian');
 console.log(team.length <= 4 ? `Team size: ${team.length}`: 'Too many members on your team')
  
+const products[];
+const product = products[0];
