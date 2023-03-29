ecf296b991e0a7843c64960ca3a66e9e4c1f1054
conorscruff <122243430+conorscruff@users.noreply.github.com>
2023-03-29 14:22:27 +0800

    Update advanced-arrow.js

--- advanced-arrow.js
+++ advanced-arrow.js
@@ -46,4 +46,20 @@ catch(e){
 console.log('catch block is running.')
 }
 
-const gradeCalc = function (score, totalScore){ const percent = (score/totalScore) * 100 let letterGrade='' if(percent>=90) {letterGrade = 'A'} else if(percent >= 80){ letterGrade = 'B'} else if(percent>= 70){ letterGrade = 'C' } else if(percent >= 60) { letterGrade = 'D'} else {letterGrade = 'F' } return `You got a ${letterGrade} (${percent}%)!`} const result = gradeCalc(9, 20) console.log(result)
+const gradeCalc = function (score, totalScore){
+const percent = (score/totalScore) * 100 
+let letterGrade='' 
+if(percent>=90) {letterGrade = 'A'} 
+  else if(percent >= 80){ 
+    letterGrade = 'B'
+  } 
+  else if(percent>= 70){
+    letterGrade = 'C' 
+  } 
+  else if(percent >= 60) {
+    letterGrade = 'D'
+  } 
+  else {letterGrade = 'F' } 
+  return `You got a ${letterGrade} (${percent}%)!`
+}
+const result = gradeCalc(9, 20) console.log(result)
